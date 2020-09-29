a1a061005b8a0cb257aee56f3e46d1f9228c444c
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-29 15:30:21 -0400

    add block tests

--- block.test.js
+++ block.test.js
@@ -1,21 +1,59 @@
-const Block = require("./block")
+const Block = require("./block");
+const { GENESIS_DATA } = require("./config");
 
-describe('Block', () => {
-    const timestamp = 'a-data'
-    const lastHash = 'foo-hash'
-    const hash = 'bar-hash'
-    const data = ['blockchain', 'data']
-    const block = new Block({
-        timestamp,
-        lastHash,
-        hash,
-        data
+describe("Block", () => {
+  const timestamp = "a-data";
+  const lastHash = "foo-hash";
+  const hash = "bar-hash";
+  const data = ["blockchain", "data"];
+  const block = new Block({
+    timestamp,
+    lastHash,
+    hash,
+    data,
+  });
+
+  it("hash a timestamp, lastHash, hash, and data property", () => {
+    expect(block.timestamp).toEqual(timestamp);
+    expect(block.lastHash).toEqual(lastHash);
+    expect(block.hash).toEqual(hash);
+    expect(block.data).toEqual(data);
+  });
+
+  describe("genesis()", () => {
+    const genesisBlock = Block.genesis();
+
+    it("returns a Block instance", () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+
+    it("returns the gesis data", () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
+
+  describe("mineBlock()", () => {
+    const lastBlock = Block.genesis();
+    const data = "mined data";
+    const minedBlock = Block.mineBlock({
+      lastBlock,
+      data,
+    });
+
+    it("returns a Block instance", () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it('sets the `lastHash` to be the `hash` of the lastBlock', ()=>{
+        expect(minedBlock.lastHash).toEqual(lastBlock.hash)
     })
 
-    it('hash a timestamp, lastHash, hash, and data property', ()=>{
-        expect(block.timestamp).toEqual(timestamp)
-        expect(block.lastHash).toEqual(lastHash)
-        expect(block.hash).toEqual(hash)
-        expect(block.data).toEqual(data)
+    it('sets the `data`', ()=>{
+        expect(minedBlock.data).toEqual(data)
     })
-})
+
+    it('set a `timestamp`', ()=>{
+        expect(minedBlock.timestamp).not.toEqual(undefined)
+    })
+  });
+});
