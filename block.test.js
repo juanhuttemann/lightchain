da3e6482ab0d977c6ed95313a8dc1bfe8772f151
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-30 20:51:15 -0400

    add crypto hash to block test

--- block.test.js
+++ block.test.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptoHash = require('./crypto-hash')
 const { GENESIS_DATA } = require("./config");
 
 describe("Block", () => {
@@ -44,16 +45,20 @@ describe("Block", () => {
       expect(minedBlock instanceof Block).toBe(true);
     });
 
-    it('sets the `lastHash` to be the `hash` of the lastBlock', ()=>{
-        expect(minedBlock.lastHash).toEqual(lastBlock.hash)
+    it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash)
     })
 
-    it('sets the `data`', ()=>{
-        expect(minedBlock.data).toEqual(data)
+    it('sets the `data`', () => {
+      expect(minedBlock.data).toEqual(data)
     })
 
-    it('set a `timestamp`', ()=>{
-        expect(minedBlock.timestamp).not.toEqual(undefined)
+    it('set a `timestamp`', () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined)
+    })
+
+    it('creates a SHA-256 `hash` based on the proper inputs', () => {
+      expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data))
     })
   });
 });
