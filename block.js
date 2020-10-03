ebfaed2c4f443ea0d72b5e6edc25e003acc3c47c
Juan Huttemann <juanfhuttemann@gmail.com>
2020-10-03 19:31:28 -0400

    add dynamic difficulty to block

--- block.js
+++ block.js
@@ -1,12 +1,15 @@
-const { GENESIS_DATA } = require("./config");
+const { GENESIS_DATA, MINE_RATE } = require("./config");
 const cryptoHash = require("./crypto-hash");
+const hexToBinary = require("hex-to-binary")
 
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
   }
 
   static genesis() {
@@ -14,15 +17,38 @@ class Block {
   }
 
   static mineBlock({ lastBlock, data }) {
-    const timestamp = Date.now()
     const lastHash = lastBlock.hash
+    let hash, timestamp;
+    let { difficulty } = lastBlock
+    let nonce = 0
+
+    do {
+      nonce++
+      timestamp = Date.now()
+      difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp })
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
+    } while (hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty))
 
     return new this({
       timestamp,
       lastHash,
       data,
-      hash: cryptoHash(timestamp, lastHash, data),
+      difficulty,
+      nonce,
+      hash
     });
+  }
+
+  static adjustDifficulty({ originalBlock, timestamp }) {
+    const { difficulty } = originalBlock
+
+    if (difficulty < 1) return 1
+
+    const difference = timestamp - originalBlock.timestamp
+
+    if (difference > MINE_RATE) return difficulty - 1
+
+    return difficulty + 1
   }
 }
 
