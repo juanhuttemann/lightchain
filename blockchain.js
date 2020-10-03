cb1776b41d0d1951a2ccd95a6ea3ce4ec5d0ec16
Juan Huttemann <juanfhuttemann@gmail.com>
2020-10-03 19:32:46 -0400

    fix isValidChain method

--- blockchain.js
+++ blockchain.js
@@ -34,15 +34,19 @@ class Blockchain {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false
 
     for (let i = 1; i < chain.length; i++) {
-      const { timestamp, lastHash, hash, data } = chain[i]
+      const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i]
 
       const actualLastHash = chain[i - 1].hash
 
+      const lastDifficulty = chain[i - 1].difficulty
+
       if (lastHash !== actualLastHash) return false
 
-      const validatedHash = cryptoHash(timestamp, lastHash, data)
+      const validatedHash = cryptoHash(timestamp, lastHash, nonce, difficulty, data)
 
       if (hash !== validatedHash) return false
+
+      if (Math.abs(lastDifficulty - difficulty) > 1) return false
     }
 
     return true
